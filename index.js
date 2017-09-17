953e05818014884e2e0071dcdaa705bc17d00e1c
Denny Hollick <dennyhollick@gmail.com>
2017-09-16 18:40:28 -0700

    Refactored to clean up code and modularize

--- index.js
+++ index.js
@@ -1,198 +1,29 @@
 /*  eslint max-len: 0 */
+const methods = require('./actions.js');
 
-let autobots = [];
-let deceptacons = [];
-let battleNumber = 0;
-let autobotScore = 0;
-let deceptaconScore = 0;
-
-// HELPER FUNCTIONS
-
-// Creates new array of bots with their overall score added to object
-
-function calculateOverallRating(bots) {
-  const botsWithRatings = [];
-  bots.forEach((bot) => {
-    const currentBot = bot; // Declared currentBot because of es-lint no-param-reassign
-    const skills = bot.skills;
-    const overallRating = (skills.strength + skills.intelligence + skills.speed + skills.endurance + skills.firepower);
-    currentBot.overallRating = overallRating;
-    botsWithRatings.push(currentBot);
-  });
-  return botsWithRatings;
-}
-
-// Sorts each team by overallRating
-
-function sortTeamByOverallRating(team) {
-  team.sort((a, b) => parseFloat(a.overallRating) - parseFloat(b.overallRating));
-  return team;
-}
-
-// Moves all bots into teams, then sorts.
-
-function sortTeam(bots, team) {
-  const currentTeam = [];
-  if (bots) {
-    bots.forEach((bot) => {
-      if (bot.team === team) {
-        currentTeam.push(bot);
-      }
-    });
-  }
-  if (currentTeam) {
-    const sortedTeam = sortTeamByOverallRating(currentTeam);
-    return sortedTeam;
-  } return false;
-}
-
-// Error checking to make sure there are both teams present
-
-function checkBothTeamsExist(team1, team2) {
-  if (team1 && team2 && team1.length && team2.length) {
-    return true;
-  }
-  return false;
-}
-
-// Checks to see if a bot should run or not.
-
-function shouldFirstBotRun(bot1, bot2) {
-  if (((bot2.skills.courage - bot1.skills.courage) >= 4) && (bot2.skills.strength - bot1.skills.strength) >= 3) {
-    return true;
-  }
-  return false;
-}
-
-function shouldSecondBotRun(bot1, bot2) {
-  if (((bot1.skills.courage - bot2.skills.courage) >= 4) && (bot1.skills.strength - bot2.skills.strength) >= 3) {
-    return true;
-  }
-  return false;
-}
-
-// Checks to see if anyone lost the battle after fighting
-
-function checkLoser(bot1, bot2) {
-  if (bot1.skills.strength - bot2.skills.strength >= 3) {
-    return true;
-  }
-  return false;
-}
-
-// Returns the winner based on the score. In a tie, returns false.
-
-function determineWinners(autobotScore, deceptaconScore, autobots, deceptacons) {
-  if (autobotScore > deceptaconScore) {
-    return autobots;
-  } else if (deceptaconScore > autobotScore) {
-    return deceptacons;
-  } return false;
-}
-
-// Moves the survivor names into a string to be returned part of the final return statement in the battle.
-
-function determineSurvivors(survivors) {
-  let survivorsNames = 'no survivors';
-  if (survivors.length) {
-    survivorsNames = '';
-    survivors.forEach((survivor) => {
-      survivorsNames += `${survivor.name}, `;
-    });
-    survivorsNames = survivorsNames.substring(0, survivorsNames.length - 2);
-  }
-  return survivorsNames;
-}
-
-// Checks bot data for validations to minimize any errors.
-
-function checkBotsForErrors(bots) {
-  let err = '';
-  for (let index = 0; index < bots.length; index += 1) {
-    const bot = bots[index];
-    if (!bot.name) {
-      err = 'a bot has no name';
-      return err;
-    } else if (!(bot.team === 'Autobot' || bot.team === 'Deceptacon')) {
-      err = `bot ${bot.name} has incorrect team.`;
-      return err;
-    } else if (!bot.skills) {
-      err = `bot ${bot.name} has no skills.`;
-      return err;
-    } else if (!(bot.skills.strength && bot.skills.intelligence && bot.skills.speed && bot.skills.endurance && bot.skills.firepower && bot.skills.courage && bot.skills.skill && bot.skills.rank)) {
-      err = `bot ${bot.name} is missing a skill.`;
-      return err;
-    }
-  }
-  return false;
-}
-
-// Checks to see if the fighting bot is a 'super bot'
-
-function isBotSuperBot(bot) {
-  const botName = bot.name;
-  const superBotNames = ['Optimus Prime', 'Predaking'];
-  for (let index = 0; index < superBotNames.length; index += 1) {
-    const superBotName = superBotNames[index];
-    if (botName === superBotName) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function areBothTeamsStillAlive(autobots, deceptacons) {
-  if (autobots.length > 0 && deceptacons.length > 0) {
-    return true;
-  }
-  return false;
-}
-
-function battleNumberLessThanMax(round, maxRounds) {
-  if (round < maxRounds) {
-    return true;
-  }
-  return false;
-}
-
-function generateReportFromBattle(autobotScore, deceptaconScore, autobots, deceptacons, battleNumber) {
-  let winningTeamName;
-  let winningSurvivors;
-  let losingTeamName;
-  let losingSurvivors;
-  let tieGame;
-  let tieGameSurvivors;
-
-  if (autobotScore > deceptaconScore) {
-    winningTeamName = 'Autobots';
-    winningSurvivors = determineSurvivors(autobots);
-    losingTeamName = 'Deceptacons';
-    losingSurvivors = determineSurvivors(deceptacons);
-  } else if (deceptaconScore > autobotScore) {
-    winningTeamName = 'Deceptacons';
-    winningSurvivors = determineSurvivors(deceptacons);
-    losingTeamName = 'Autobots';
-    losingSurvivors = determineSurvivors(autobots);
-  } else {
-    tieGame = true;
-    tieGameSurvivors = `with ${determineSurvivors(autobots)} remaining for Autobots and ${determineSurvivors(deceptacons)} remaining for Deceptacons!`;
-  }
-
-  if (tieGame) {
-    return (`Battles: #${battleNumber}\nIt is a tie game ${tieGameSurvivors}`);
-  }
-
-  return (
-    `Battles: #${battleNumber}\nThe winning team is the ${winningTeamName} with ${winningSurvivors} remaining\nThe losing team is the ${losingTeamName} with ${losingSurvivors} remaining`);
-}
-
+const calculateOverallRating = methods.calculateOverallRating;
+const sortTeam = methods.sortTeam;
+const checkBothTeamsExist = methods.checkBothTeamsExist;
+const shouldFirstBotRun = methods.shouldFirstBotRun;
+const shouldSecondBotRun = methods.shouldSecondBotRun;
+const checkLoser = methods.checkLoser;
+const checkBotsForErrors = methods.checkBotsForErrors;
+const isBotSuperBot = methods.isBotSuperBot;
+const generateReportFromBattle = methods.generateReportFromBattle;
+const battleNumberLessThanMax = methods.battleNumberLessThanMax;
+const areBothTeamsStillAlive = methods.areBothTeamsStillAlive;
+const killFirstBot = methods.killFirstBot;
 
 // MAIN FUNCTION
 
 // Initializes the simulation given an array of Bots
 
 function battle(bots) {
-
+  let autobots = [];
+  let deceptacons = [];
+  let battleNumber = 0;
+  let autobotScore = 0;
+  let deceptaconScore = 0;
   const botValidationErrors = checkBotsForErrors(bots);
 
   // Validate input bots have all data is clean
@@ -242,28 +73,27 @@ function battle(bots) {
 
     if (isBotSuperBot(autobotFighter)) {
       autobotScore += 1;
-      deceptacons.shift();
+      deceptacons = killFirstBot(deceptacons);
     } else if (isBotSuperBot(deceptaconFighter)) {
       deceptaconScore += 1;
-      autobots.shift();
+      autobots = killFirstBot(autobots);
     } else if (shouldFirstBotRun(autobotFighter, deceptaconFighter)) {
       deceptaconScore += 1;
-      autobots.shift();
+      autobots = killFirstBot(autobots);
     } else if (shouldSecondBotRun(autobotFighter, deceptaconFighter)) {
       autobotScore += 1;
-      deceptacons.shift();
+      deceptacons = killFirstBot(deceptacons);
     } else if (checkLoser(autobotFighter, deceptaconFighter)) {
       autobotScore += 1;
-      deceptacons.shift();
+      deceptacons = killFirstBot(deceptacons);
     } else if (checkLoser(deceptaconFighter, autobotFighter)) {
       deceptaconScore += 1;
-      autobots.shift();
+      autobots = killFirstBot(autobots);
     } else {
-      autobots.shift();
-      deceptacons.shift();
+      autobots = killFirstBot(autobots);
+      deceptacons = killFirstBot(deceptacons);
     }
   }
-
 
   // Generate the output text
 
